e5ecea39b27fb3b7589733171d6b471ac2a1e26d
Vasil <dedejski44@gmail.com>
2020-07-13 17:07:09 +0200

    Initial Commit

---
+++ fullstack-tool-backend/src/main/java/com/vasildedejski/fullstacktoolback/FullstackToolBackApplication.java
@@ -0,0 +1,13 @@
+package com.vasildedejski.fullstacktoolback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackToolBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackToolBackApplication.class, args);
+    }
+
+}
---
+++ fullstack-tool-backend/src/test/java/com/vasildedejski/fullstacktoolback/FullstackToolBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasildedejski.fullstacktoolback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackToolBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
