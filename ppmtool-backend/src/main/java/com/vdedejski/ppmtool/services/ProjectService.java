c0b0313fd5004fb5cd9c78ddab04f472b800204c
Vasil <dedejski44@gmail.com>
2020-07-14 19:05:34 +0200

    Same projectIdentifier exception handling process

--- ppmtool-backend/src/main/java/com/vdedejski/ppmtool/services/ProjectService.java
+++ ppmtool-backend/src/main/java/com/vdedejski/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.vdedejski.ppmtool.services;
 
 import com.vdedejski.ppmtool.domain.Project;
+import com.vdedejski.ppmtool.exceptions.ProjectIdException;
 import com.vdedejski.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,16 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
+    public Project saveOrUpdateProject(Project project) {
 
         // Logic to save users.. makeing sure to update operation
 
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+
+        }
     }
 }
