72255deb758e7cde585071fa6be774f155e9e7f9
Vasil <dedejski44@gmail.com>
2020-07-14 15:59:22 +0200

    H2 Database init

---
+++ ppmtool-backend/src/main/java/com/vdedejski/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.vdedejski.ppmtool.repositories;
+
+import com.vdedejski.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+    
+}
