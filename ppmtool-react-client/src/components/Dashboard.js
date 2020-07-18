6b2d3beb4bdb081acacec20f42fa3f6ec7d0ef6e
Vasil <dedejski44@gmail.com>
2020-07-18 17:16:15 +0200

    Layout and Project Componenets started

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1 className="mr-1">Welcome to Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
